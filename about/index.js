a36c078233b618d1feb626bead499de58421c78e
Smalllong <898310778@qq.com>
2022-05-15 16:21:24 +0800

    cleanup, add winbox and about

---
+++ about/index.js
@@ -0,0 +1,32 @@
+var L = Lightue
+
+new L({
+  name: {
+    $tag: 'h1',
+    $$: 'Smalllong',
+  },
+  details: [
+    'experienced frontend developer located in Shanghai',
+    {
+      $$: 'github:',
+      link: L.a({
+        _href: 'https://github.com/smalllong',
+        $$: 'https://github.com/smalllong',
+      }),
+    },
+    {
+      $$: 'gitee:',
+      link: L.a({
+        _href: 'https://gitee.com/smalllong',
+        $$: 'https://gitee.com/smalllong',
+      }),
+    },
+    {
+      $$: 'email:',
+      link: L.a({
+        _href: 'mailto:m18818276304@icloud.com',
+        $$: 'm18818276304@icloud.com',
+      }),
+    },
+  ],
+})
--- js/index.js
+++ js/index.js
@@ -1,35 +1,55 @@
+var L = Lightue
+
 function randomColor(initS, initL) {
-  var h = Math.floor(Math.random()*256),
-    s = initS+Math.ceil(20*Math.random()),
-    l = initL+Math.round(20*Math.random())
-  return 'hsl('+h+','+s+'%,'+l+'%)'
+  var h = Math.floor(Math.random() * 256),
+    s = initS + Math.ceil(20 * Math.random()),
+    l = initL + Math.round(20 * Math.random())
+  return 'hsl(' + h + ',' + s + '%,' + l + '%)'
 }
 
 function Tile(title, link, img) {
+  var color = randomColor(80, 25)
   return {
-    $tag: 'a',
-    _href: link,
-    _style: 'background-color: '+randomColor(80, 25),
-    tileTitle: title,
-    tileImg: img?{
-      $tag: 'img',
-      _src: img,
-    }:undefined,
+    _style: 'background-color: ' + color,
+    tileTitle: {
+      $tag: 'a',
+      _href: link,
+      $$: title,
+    },
+    tileImg: img
+      ? {
+          $tag: 'img',
+          _src: img,
+        }
+      : undefined,
+    onclick: (e) => {
+      if (e.target.tagName == 'A') return
+      e.preventDefault()
+      new WinBox(title, {
+        url: link,
+        width: 640,
+        height: 480,
+        x: 'center',
+        y: 'center',
+        border: 4,
+        background: color,
+      })
+    },
   }
 }
 
-var vm = Lightue({
-  _style: 'background-image: linear-gradient('+randomColor(20, 10)+','+randomColor(20, 10)+')',
+L({
+  _style: 'background-image: linear-gradient(' + randomColor(20, 10) + ',' + randomColor(20, 10) + ')',
   tiles: [
-    Tile('Skyscrapers Map in Shanghai', 'sh-skyscrapers/index.html'),
+    Tile('Simple sheet editor', 'spreadsheet/index.html', 'spreadsheet/index.png'),
+    Tile('Skyscrapers Map in Shanghai', 'sh-skyscrapers/index.html', 'sh-skyscrapers/index.png'),
     Tile('get your browser information', 'browser/index.html', 'browser/index.png'),
     Tile('a simple digital clock', 'clock/index.html', 'clock/index.png'),
-    Tile('different behavior of 4 center methods', 'center/index.html', 'center/index.png'),
+    Tile('different behavior of 5 center methods', 'center/index.html', 'center/index.png'),
     Tile('Polyline Simplify on a Gaode Map', 'polylineSimplify/index.html', 'polylineSimplify/index.png'),
     Tile('Zoomable full screen photo viewer', 'photoViewer/index.html'),
     Tile('Simple polygon test', 'simplePolygon/index.html'),
-    Tile('Chat', 'chat/index.html'),
-    Tile('Simple sheet editor', 'spreadsheet/index.html'),
     Tile('A colorful ball fountain with motion blur', 'fountain/index.html'),
-  ]
+    Tile('About me', 'about/index.html'),
+  ],
 })
